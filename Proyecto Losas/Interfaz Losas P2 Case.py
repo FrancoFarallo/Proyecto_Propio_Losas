e02ee0aa80281aecee641d62cfeb0ae9eb1aa06b
Franco <franco.farallo@gmail.com>
2021-10-13 14:32:07 -0300

    Iteración de las funciones y los import

--- Proyecto Losas/Interfaz Losas P2 Case.py
+++ Proyecto Losas/Interfaz Losas P2 Case.py
@@ -1,5 +1,7 @@
 from tkinter import *
+
 import tkinter
+
 raiz = Tk()
 raiz.title("Proyecto Losas")
 raiz.config(bg="black")
@@ -10,6 +12,8 @@ texto_bienvenida=Label (frame_1, text="¡Bienvenido a X!", font="20", fg="white"
 texto_bienvenida.place(x=335, y=220)
 indicacion_inicial=Label (frame_1, text="Pulse siguiente para continuar", font=15, fg="white", bg="black", justify="center")
 indicacion_inicial.place(x=300, y=270)
+
+
 def siguiente ():
     raiz.withdraw()
     frame_2=tkinter.Toplevel(bg="black", width="800", height="500")
@@ -31,9 +35,13 @@ def siguiente ():
     entrada_bajo_l.place(x=410, y=350)
     entrada_vol=Entry(frame_2, font="20", fg="Black", bg="white")
     entrada_vol.place(x=600, y=350)
+   
+   
     def siguiente2 ():
         raiz.withdraw()
         frame_3=tkinter.Toplevel(bg="black", width="800", height="500") 
+        
+        
         def switch ():
             switcher=[
                 1:if int(entrada_comedor.get()) >= 1 : #Mismo proceso para todos
@@ -60,6 +68,8 @@ def siguiente ():
             for numero_de_losas in range (1, int(entrada_comedor.get())): #Nota: Con el ciclo While crashea
                 tx_cantidad_losas_comedor=Label (frame_3, text="Losas de Comedor y Hab", font="20", fg="white", bg="black")
                 tx_cantidad_losas_comedor.place(x=100, y=50)
+               
+               
                 def siguiente3():
                      raiz.withdraw()
                      frame_3=tkinter.Toplevel(bg="black", width="800", height="500") 
